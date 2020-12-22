f441bb3b65bfdf07169fb79ff7f7b96b33472fb8
Maximiliano Bartolozzi <mxbarto@gmail.com>
2020-12-22 13:57:07 -0300

    first attempt  of creating a dashboard menu

--- wp-content/themes/barto-theme/inc/function-admin.php
+++ wp-content/themes/barto-theme/inc/function-admin.php
@@ -9,19 +9,25 @@ ADMIN PAGE
 */
 
 function barto_add_admin_page(){
-    add_menu_page( 
-        'Barto Theme Options', 
-        'Barto', 
-        'manage_options', 
-        'mxbarto',
-        'barto_theme_create_page',
-        'dashicons-editor-bold',
-        110
-    );
+    // Generate Barto Admin Page
+    add_menu_page( 'Barto Theme Options', 'Barto', 'manage_options', 'mxbarto','barto_theme_create_page','dashicons-editor-bold', 110 );
+
+    // Generate Barto Admin Subpage
+    add_submenu_page('mxbarto', 'Barto Settings', 'Settings', 'manage_options', 'mxbarto_settings', 'mxbarto_theme_settings_page');
+
+    add_submenu_page( 'mxbarto', 'CSS Settings', 'CSS', 'manage_options', 'mxbarto_css', 'mxbarto_theme_css_page', );
 }
 
 add_action('admin_menu','barto_add_admin_page');
 
 function barto_theme_creatye_page(){
+
+}
+
+function mxbarto_theme_settings_page(){
+    
+}
+
+function mxbarto_theme_css_page(){
 
 }
