441c0156f5193f3e8420106ccbe9a3034bd312e9
Maximiliano Bartolozzi <mxbarto@gmail.com>
2020-12-23 09:38:21 -0300

    simple custom options with settings API

--- wp-content/themes/barto-theme/inc/function-admin.php
+++ wp-content/themes/barto-theme/inc/function-admin.php
@@ -12,10 +12,16 @@ function barto_add_admin_page(){
     // Generate Barto Admin Page
     add_menu_page( 'Barto Theme Options', 'Barto', 'manage_options', 'mxbarto','barto_theme_create_page','dashicons-editor-bold', 110 );
 
-    // Generate Barto Admin Subpage
+    // Generate Barto Admin Subpages
+    // Settings
     add_submenu_page('mxbarto', 'Barto Theme Options', 'Settings', 'manage_options', 'mxbarto', 'mxbarto_theme_create_page');
-
+    // CSS
     add_submenu_page( 'mxbarto', 'CSS Settings', 'CSS', 'manage_options', 'mxbarto_css', 'mxbarto_theme_css_page', );
+
+    //Activate custom settings
+    add_action( 'admin_init', 'mxbarto_custom_settings' );
+
+
 }
 
 add_action('admin_menu','barto_add_admin_page');
@@ -26,4 +32,13 @@ function barto_theme_create_page(){
 
 function mxbarto_theme_css_page(){
 
+}
+
+function mxbarto_custom_settings(){
+    register_setting( 'mxbarto-settings-group', 'first_name'  );
+    add_settings_section( 'mxbarto-sidebar-options', 'Sidebar Options', 'mxbarto_sidebar_options', 'mxbarto' );
+}
+
+function mxbarto_sidebar_options(){
+    echo 'Customize your sidebar information';
 }
--- wp-content/themes/barto-theme/inc/templates/barto-admin.php
+++ wp-content/themes/barto-theme/inc/templates/barto-admin.php
@@ -1,4 +1,8 @@
 <h1>Barto Template Options</h1>
 <h3>Manage Options</h3>
 <p>Customize the default Worpdress Options</p>
+<form action="" method="post">
+    <?php settings_fields( 'mxbarto-settings-group' ); ?>
+    <?php do_settings_sections( 'mxbarto' ); ?>
+</form>
 
